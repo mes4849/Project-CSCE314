b8275e2711c4819e3c7f0c578ede65553a50738a
Martin <mes4849@tamu.edu>
2019-04-24 23:07:32 -0400

    Format

--- javafit/src/Swing.java
+++ javafit/src/Swing.java
@@ -6,24 +6,20 @@ public class Swing {
     private final JFrame thisFrame;
     private final ArrayList<JButton> buttons = new ArrayList<JButton>();
 
+    // constructor needs a JFrame to add the buttons to
+        // I have no idea if we were supposed to be adding items directly to the frame?
+        // Anyway this works.
     public Swing(JFrame parentFrame) {
         thisFrame = parentFrame;
     }
 
-
     public void addButton(int i , String S) {
         buttons.add(new JButton(S));
         thisFrame.add(buttons.get(i));
-        buttons.get(i).setBounds(25 ,10 + (i*120),550,110);
-    }
-
-    public void setButtonText(int i , String S) {
-        buttons.get(i).setText(S);
+        buttons.get(i).setBounds(25 ,10 + (i*120),550,110);     // hard coded for three buttons
     }
 
     public ArrayList<JButton> getButtons() {
         return buttons;
     }
-
-
 }
--- javafit/src/javaFit.java
+++ javafit/src/javaFit.java
@@ -14,18 +14,15 @@ public class javaFit {
 		
 	// Load data into app
     Workouts workouts = FileAccess.loadWorkouts();
-
-
     EnumMap<Config.MuscleGroup, ArrayList<Config.Muscle>> muscleGroups = FileAccess.loadFormats();
 	  
   	// Create Screen Handler
   	MainWindow mainScreen = new MainWindow(workouts, muscleGroups);
-
   	// Create Swing object??
 	Swing buttonSwing = new Swing(mainScreen.getFrame());
 
-
-	mainScreen.startProgram(buttonSwing);
+	// start the program
+	mainScreen.startProgram(buttonSwing);		// we pass in the swing object to register all the buttons it contains doing it this way allows us to have different buttons registered to different frame
 	  
 	  
 
