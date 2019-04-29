0aeaa13faaee4673f4c9e21f141b15afebbfd828
Martin <mes4849@tamu.edu>
2019-04-29 20:05:21 +0000

    Revert javaFit to original from Glen

--- javafit/src/javaFit.java
+++ javafit/src/javaFit.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
 import java.util.EnumMap;
 
+/**
+ * 
+ */
 
+/**
+ * @author Glen
+ *
+ */
 
 public class javaFit {
 
@@ -9,20 +16,13 @@ public class javaFit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-	System.out.println("Just a test");
 		
-	// Load data into app
-    Workouts workouts = FileAccess.loadWorkouts();
+	  // Load data into app
+    Workouts workouts= FileAccess.loadWorkouts();
     EnumMap<Config.MuscleGroup, ArrayList<Config.Muscle>> muscleGroups = FileAccess.loadFormats();
 	  
-  	// Create Screen Handler
-  	MainWindow mainScreen = new MainWindow(workouts, muscleGroups);
-  	// Create Swing object??
-	Swing buttonSwing = new Swing(mainScreen.getFrame());
-
-	// start the program
-	mainScreen.startProgram(buttonSwing);		// we pass in the swing object to register all the buttons it contains doing it this way allows us to have different buttons registered to different frame
+	  // Create Screen Handler
+	  MainWindow mainScreen = new MainWindow(workouts, muscleGroups);
 	  
 	  
 
